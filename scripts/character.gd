562578bccac96776f1df71d1e62ee8e31ae9b093
MedusaTea <medusastea@gmail.com>
2026-04-18 11:10:22 -0500

    game is coming together, duck, jump, still a lot to do

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+@onready var rigidBody = $RigidBody3D
+
 @export var sideStopRange = 5
 
 @export var duckTimerMax = 2.0
@@ -10,12 +12,14 @@ extends Node3D
 func _physics_process(delta: float) -> void:
 	duckTimer -= delta
 	if ducking and duckTimer < 0:
-		position.y = 0
+		rigidBody.position.y = 0
+		rigidBody.collision_mask = 7
+		rigidBody.gravity_scale = 1.0
 		ducking = false
 		duckTimer = duckTimerMax
 		
 	if ducking:
-		position.y = -1
+		rigidBody.position.y = -1
 		
 func _on_body_entered(body: Node) -> void:
 	print(body.name)
@@ -29,12 +33,14 @@ func handleAction(name) -> void:
 		if position.x < sideStopRange:
 			position += Vector3(3, 0, 0)
 
-	elif name.contains('jump'):
-		if abs(position.y) < 2:
-			position += Vector3(0, 4, 0)
+	elif name.contains('jump') and !ducking:
+		if abs(rigidBody.position.y) < 2:
+			rigidBody.position += Vector3(0, 4, 0)
 
 	elif name.contains('duck') and !ducking:
-		if abs(position.y) < 2:
+		if abs(rigidBody.position.y) < 2:
+			rigidBody.collision_mask = 3
+			rigidBody.gravity_scale = 0.0
 			ducking = true
 	
 	elif name.contains('swim'):
